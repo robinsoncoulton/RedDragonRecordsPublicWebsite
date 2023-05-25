73c562a55f2309eacd046138d8744ff8b12a6a21
Robinson Coulton <robinsoncoulton@Robinsons-MacBook-Pro.local>
2023-05-25 12:34:27 +0800

    update build

--- src/Utils/routes.tsx
+++ src/Utils/routes.tsx
@@ -1,33 +1,35 @@
 
-import Home from '../Pages/Home';
-import Contact from "../Pages/Contact";
-import Gallery from "../Pages/Gallery";
-import Portfolio from "../Pages/Portfolio";
-import Error from "../Pages/Error";
+// import Home from '../Pages/Home';
+// import Contact from "../Pages/Contact";
+// import Gallery from "../Pages/Gallery";
+// import Portfolio from "../Pages/Portfolio";
+// import Error from "../Pages/Error";
+import Construction from '../Components/Construction';
 
 export const baseRoutes = [
     {
         path: '/',
-        element: <Home/>
+        element: <Construction/>
+        // element: <Home/>
     },
-    {
-        path: 'Gallery',
-        element: <Gallery/>
-    },
-    {
-        path: 'Portfolio',
-        element: <Portfolio/>
-    },
-    {
-        path: 'Contact',
-        element: <Contact/>
-    },
+    // {
+    //     path: 'Gallery',
+    //     element: <Gallery/>
+    // },
+    // {
+    //     path: 'Portfolio',
+    //     element: <Portfolio/>
+    // },
+    // {
+    //     path: 'Contact',
+    //     element: <Contact/>
+    // },
 ]
 export const routes = [
     ...baseRoutes,
     {
         path: '*',
-        element: <Error/>
+        element: <Construction/>
     },
 ]
 
